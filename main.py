2b71f44b15001bd88d289055bc2bb2d153611fd5
Jake <calcuttj@msu.edu>
2022-09-30 08:45:58 -0700

    Added graphics

--- main.py
+++ main.py
@@ -3,6 +3,26 @@ import numpy as np
 import sys
 import random
 
+class Tile(pygame.sprite.Sprite):
+  def __init__(self, value, pos):
+    pygame.sprite.Sprite.__init__(self)
+    self.image = pygame.image.load("%itile.png"%value)
+    self.rect = self.image.get_rect()
+    self.rect.left = (pos[1]*140 + (pos[1]+1)*10)
+    self.rect.top = (pos[0]*140 + (pos[0]+1)*10)
+    #TODO -- add valid list of numbers
+
+def clear_tiles_from_board(the_group, the_screen):
+  #the_group.clear(the_screen, (0, 0, 0))
+  the_group.empty()
+  
+
+def make_tiles(board, the_group):
+  for i in range(len(board)):
+    for j in range(len(board)):
+      if board[i][j] > 0:
+        the_group.add(Tile(board[i][j], (i, j)))
+
 def init_board():
   board = np.zeros((4,4))
   p1 = (random.randint(0, 3), random.randint(0, 3))
@@ -102,6 +122,9 @@ if __name__ == '__main__':
   print(the_board)
 
   screen = pygame.display.set_mode(size)
+  all_sprites = pygame.sprite.RenderUpdates()
+  make_tiles(the_board, all_sprites)
+  #first_tile = Tile(2, (0, 0))
   #ball = pygame.image.load("intro_ball.gif")
   #ballrect = ball.get_rect()
 
@@ -114,6 +137,7 @@ if __name__ == '__main__':
         if not (can_shift_left(the_board) or can_shift_right(the_board) or
                 can_shift_up(the_board) or can_shift_down(the_board)):
           print('YOU LOSE')
+
         if np.any(the_board == 2048):
           print('YOU WIN')
           continue
@@ -121,21 +145,27 @@ if __name__ == '__main__':
           print('Left')
           the_board = shift_left(the_board)
           spawn(the_board)
-        if event.key == pygame.K_RIGHT and can_shift_right(the_board):
+        elif event.key == pygame.K_RIGHT and can_shift_right(the_board):
           print('Right')
           the_board = shift_right(the_board)
           spawn(the_board)
-        if event.key == pygame.K_UP and can_shift_up(the_board):
+        elif event.key == pygame.K_UP and can_shift_up(the_board):
           print('Up')
           the_board = shift_up(the_board)
           spawn(the_board)
-        if event.key == pygame.K_DOWN and can_shift_down(the_board):
+        elif event.key == pygame.K_DOWN and can_shift_down(the_board):
           print('Down')
           the_board = shift_down(the_board)
           spawn(the_board)
+        else: continue
+
         print(the_board)
         print(score)
         print()
-    #screen.fill(black)
-    #screen.blit(ball, ballrect)
-    #pygame.display.flip()
+
+    screen.fill(black)
+    #screen.blit(first_tile.tile, first_tile.rect)
+    clear_tiles_from_board(all_sprites, screen)
+    make_tiles(the_board, all_sprites)
+    all_sprites.draw(screen)
+    pygame.display.flip()
