19feea95874a677ae36bf7924c1c408a056ab18b
Jake <calcuttj@msu.edu>
2022-10-01 16:24:11 -0700

    Replacing the tiles loaded from images with drawing rectangles and rendering text in the same spot. Added lots of comments. Shifting where the tiles are drawn so there's some space on the right and bottom

--- main.py
+++ main.py
@@ -4,33 +4,42 @@ import sys
 import random
 
 class Tile(pygame.sprite.Sprite):
-  def __init__(self, value, pos):
+  def __init__(self, value, pos, screen):
     pygame.sprite.Sprite.__init__(self)
-    self.image = pygame.image.load("%itile.png"%value)
-    self.rect = self.image.get_rect()
-    self.rect.left = (pos[1]*140 + (pos[1]+1)*10)
-    self.rect.top = (pos[0]*140 + (pos[0]+1)*10)
-    #TODO -- add valid list of numbers
 
+    #Draw white rectangle on the screen
+    self.white_rect = pygame.draw.rect(
+        screen, (255, 255, 255),
+        pygame.Rect((pos[1]*140 + (pos[1]+1)*8),
+        (pos[0]*140 + (pos[0]+1)*8),
+        140, 140))
+    self.font = pygame.font.SysFont('Arial', 80)
+
+    #The font rendering will be the image of the sprite
+    #Set its center to the above rectangle
+    self.image = self.font.render('%i'%value, True, (0, 0, 0))
+    self.rect = self.image.get_rect()
+    self.rect.center = self.white_rect.center
+
+
+##Might just remove this unless it ends up being more complicated
 def clear_tiles_from_board(the_group, the_screen):
-  #the_group.clear(the_screen, (0, 0, 0))
   the_group.empty()
   
 
-def make_tiles(board, the_group):
+#Make a tile on the board wherever there is a number > 0
+def make_tiles(board, the_group, screen):
   for i in range(len(board)):
     for j in range(len(board)):
       if board[i][j] > 0:
-        the_group.add(Tile(board[i][j], (i, j)))
+        the_group.add(Tile(board[i][j], (i, j), screen))
 
+#Make all zeros on the board -- spawn tiles on two random spaces 
 def init_board():
   board = np.zeros((4,4))
-  p1 = (random.randint(0, 3), random.randint(0, 3))
   spawn(board)
   spawn(board)
   return board
-
-#def draw_board(screen, board):
 
 def can_shift_up(board):
   rotated_board = np.rot90(board)
@@ -38,8 +47,8 @@ def can_shift_up(board):
 
 def shift_up(board):
   rotated_board = np.rot90(board) 
-  shifted_board = shift_left(rotated_board)
-  return np.rot90(shifted_board, -1)
+  shifted_board, score = shift_left(rotated_board)
+  return (np.rot90(shifted_board, -1), score)
 
 def can_shift_down(board):
   rotated_board = np.rot90(board, -1)
@@ -47,8 +56,8 @@ def can_shift_down(board):
 
 def shift_down(board):
   rotated_board = np.rot90(board, -1) 
-  shifted_board = shift_left(rotated_board)
-  return np.rot90(shifted_board)
+  shifted_board, score = shift_left(rotated_board)
+  return (np.rot90(shifted_board), score)
 
 def can_shift_right(board):
   flipped_board = np.flip(board, axis=1)
@@ -56,9 +65,14 @@ def can_shift_right(board):
 
 def shift_right(board):
   flipped_board = np.flip(board, axis=1)
-  shifted_board = shift_left(flipped_board)
-  return np.flip(shifted_board, axis=1)
+  shifted_board, score = shift_left(flipped_board)
+  return (np.flip(shifted_board, axis=1), score)
 
+#Check each row for the following
+#  If all zeros -- row is unshiftable
+#  If any pair -- row is shiftable
+#  If any zero on left of numbered spot -- row is shiftable
+#If any row is shiftable -- the whole board is shiftable
 def can_shift_left(board):
   shiftable = []
   for row in board:
@@ -67,15 +81,15 @@ def can_shift_left(board):
     else:
       row_can_shift = False
       for i in range(1, len(row)):
-        #print(row[i-1], row[i])
         if row[i-1] == row[i] and row[i] > 0:
           row_can_shift = True
         if row[i-1] == 0 and row[i] > 0:
           row_can_shift = True
       shiftable.append(row_can_shift)
   return np.any(shiftable)
-  #print(shiftable)
 
+#Spawn a 2 or 4 (10% chance)
+#at a random spot on the board where the space is zero
 def spawn(board):
   if np.any(board == 0):
     i = random.choice(np.where(np.any(board == 0, axis=1))[0]) 
@@ -84,26 +98,30 @@ def spawn(board):
 
 def shift_left(board):
   new_board = []
+  score = 0
   for row in board:
-    trimmed_row = row[row != 0]
-    #print(trimmed_row)
+    trimmed_row = row[row != 0] #remove zeroes 
 
     i = 1
     the_len = len(trimmed_row)
+    #move from left to right through the row
     while i < the_len:
-    #for i in range(1, len(trimmed_row)):
-      #print(trimmed_row[i-1], trimmed_row[i])
-      if trimmed_row[i-1] == trimmed_row[i]:
-        trimmed_row[i-1] *= 2
-        trimmed_row = np.delete(trimmed_row, i)
-      i += 1
+      #check if there is a pair
+      if trimmed_row[i-1] == trimmed_row[i]: 
+        trimmed_row[i-1] *= 2 # double it
+        score += trimmed_row[i-1] # add to score
+        trimmed_row = np.delete(trimmed_row, i) #remove the right tile
+      i += 1 #skip so we don't double count
       the_len = len(trimmed_row)
-        #break
 
-    padded_row = np.pad(trimmed_row, (0, len(row)-len(trimmed_row)), 'constant', constant_values=(0, 0))
-    #print(padded_row)
-    new_board.append(padded_row)
-  return np.array(new_board)
+    #add zeros back in on the right
+    #padded_row = np.pad(
+    #    trimmed_row, (0, len(row)-len(trimmed_row)), 'constant', 
+    #    constant_values=(0, 0))
+    new_board.append(np.pad(
+        trimmed_row, (0, len(row)-len(trimmed_row)), 'constant', 
+        constant_values=(0, 0)))
+  return (np.array(new_board), score)
 
 if __name__ == '__main__':
   pygame.init()
@@ -112,20 +130,24 @@ if __name__ == '__main__':
   black = 0, 0, 0
   speed = [0, 0]
 
-  #the_board = np.zeros(4, 4)
+  #Initial setup
   the_board = init_board()
   print(the_board)
-
   screen = pygame.display.set_mode(size)
   all_sprites = pygame.sprite.RenderUpdates()
-  make_tiles(the_board, all_sprites)
-  #first_tile = Tile(2, (0, 0))
-  #ball = pygame.image.load("intro_ball.gif")
-  #ballrect = ball.get_rect()
+  make_tiles(the_board, all_sprites, screen)
 
-  score = 0
+  #Initial drawing
+  screen.fill(black)
+  clear_tiles_from_board(all_sprites, screen)
+  make_tiles(the_board, all_sprites, screen)
+  all_sprites.draw(screen)
+  pygame.display.flip()
+
+  total_score = 0
 
   while True:
+    update = False
     for event in pygame.event.get():
       if event.type == pygame.QUIT: sys.exit()
       elif event.type == pygame.KEYDOWN:
@@ -138,29 +160,37 @@ if __name__ == '__main__':
           continue
         if event.key == pygame.K_LEFT and can_shift_left(the_board):
           print('Left')
-          the_board = shift_left(the_board)
+          the_board, score = shift_left(the_board)
+          total_score += score
           spawn(the_board)
+          update = True
         elif event.key == pygame.K_RIGHT and can_shift_right(the_board):
           print('Right')
-          the_board = shift_right(the_board)
+          the_board, score = shift_right(the_board)
+          total_score += score
           spawn(the_board)
+          update = True
         elif event.key == pygame.K_UP and can_shift_up(the_board):
           print('Up')
-          the_board = shift_up(the_board)
+          the_board, score = shift_up(the_board)
+          total_score += score
           spawn(the_board)
+          update = True
         elif event.key == pygame.K_DOWN and can_shift_down(the_board):
           print('Down')
-          the_board = shift_down(the_board)
+          the_board, score = shift_down(the_board)
+          total_score += score
           spawn(the_board)
+          update = True
         else: continue
 
         print(the_board)
-        print(score)
+        print(int(total_score))
         print()
 
-    screen.fill(black)
-    #screen.blit(first_tile.tile, first_tile.rect)
-    clear_tiles_from_board(all_sprites, screen)
-    make_tiles(the_board, all_sprites)
-    all_sprites.draw(screen)
-    pygame.display.flip()
+    if update:
+      screen.fill(black)
+      clear_tiles_from_board(all_sprites, screen)
+      make_tiles(the_board, all_sprites, screen)
+      all_sprites.draw(screen)
+      pygame.display.flip()
